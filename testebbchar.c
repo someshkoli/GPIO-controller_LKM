291fd801c464f70859249893f9f49e72efecfafc
someshkoli <kolisomesh27@gmail.com>
2020-05-12 01:09:20 +0530

    test file added

--- testebbchar.c
+++ testebbchar.c
@@ -4,10 +4,8 @@
 #include<fcntl.h>
 #include<string.h>
 #include<unistd.h>
- 
 #define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
 static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
- 
 int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
@@ -28,11 +26,10 @@ int main(){
  
    printf("Press ENTER to read back from the device...\n");
    getchar();
- 
    printf("Reading from the device...\n");
    ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
    if (ret < 0){
-      perror("Failed to read the message from the device.");
+      perror("failed to read the message from the device.");
       return errno;
    }
    printf("The received message is: [%s]\n", receive);
